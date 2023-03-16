73e49e75aec6510be58ddabd97621198d7ab173f
Israel Garcia Alvarez <igalvarez@gmail.com>
2023-03-16 10:54:40 +0100

    uy

--- mailer.py
+++ mailer.py
@@ -1,18 +1,1 @@
-import smtplib
-
-sender = 'from@example.com'
-receivers = ['israel@localhost']
-message = """From: From Person <from@example.com>
-To: To Person <to@example.com>
-Subject: SMTP email example
-
-
-This is a test message.
-"""
-
-try:
-    smtpObj = smtplib.SMTP('localhost')
-    smtpObj.sendmail(sender, receivers, message)         
-    print("Successfully sent email")
-except SMTPException:
-    pass
+print('Hello, world!')
